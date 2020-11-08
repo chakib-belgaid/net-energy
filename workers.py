87815824a83ef92c9b50450e66484938e2f73fa1
root <root@chetemi-8.lille.grid5000.fr>
2020-11-08 14:09:40 +0100

    add : decorator for making jobs lazy

--- workers.py
+++ workers.py
@@ -1,10 +1,43 @@
-from sys import argv
 import time
 import sys
 import signal
 import psutil
 import os
 import multiprocessing
+import functools
+
+
+def lazy(_func=None, *, affinity=0, charge=100):
+    if type(affinity) != list:
+        affinity = [affinity]
+
+    def decorator_lazy(worker):
+        @functools.wraps(worker)
+        def wrapper_lazy(*args, **kwargs):
+
+            psutil.Process(os.getpid()).cpu_affinity(affinity)
+            if charge == 100:
+                worker()
+                return
+
+            sleep_duration = charge / 10000
+            pause_duration = 0.0099 - sleep_duration
+            process = multiprocessing.Process(target=worker)
+            process.start()
+            psutil.Process(process.pid).cpu_affinity(affinity)
+            while process.is_alive():
+                os.kill(process.pid, signal.SIGSTOP)
+                time.sleep(pause_duration)
+                os.kill(process.pid, signal.SIGCONT)
+                time.sleep(sleep_duration)
+            return
+
+        return wrapper_lazy
+
+    if _func is None:
+        return decorator_lazy
+    else:
+        return decorator_lazy(_func)
 
 
 def watcher(affinity=0, charge=100):
@@ -26,6 +59,7 @@ def watcher(affinity=0, charge=100):
         time.sleep(sleep_duration)
 
 
+@lazy(affinity=[2, 3], charge=32)
 def worker():
     i = 1
     j = 1
@@ -45,5 +79,9 @@ def main():
         x.start()
 
 
+def main2():
+    worker()
+
+
 if __name__ == "__main__":
-    main()
+    main2()
